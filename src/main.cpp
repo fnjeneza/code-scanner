073be6fe1089058a89ec0ff626208240adcd3da5
Francois Njeneza <njenezamed@gmail.com>
2017-06-01 18:51:28 +0200

    retrieve cursor type

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,19 @@ std::ostream& operator<<(std::ostream& stream, const CXString& cx_string)
     return stream;
 }
 
+// Retrieve a type of cursor
+std::string type(const CXCursor &cursor)
+{
+    //cursor type
+    CXType type = clang_getCursorType(cursor);
+    // cursor type spelling
+    CXString spelling = clang_getTypeSpelling(type);
+    // cursor spelling as string
+    std::string spelling_str = clang_getCString(spelling);
+    // freed the CXString
+    clang_disposeString(spelling);
+    return spelling_str;
+}
 
 int main(int argc, char **argv)
 {
@@ -23,10 +36,19 @@ int main(int argc, char **argv)
             CXTranslationUnit_None);
     CXFile file = clang_getFile(unit, argv[1]);
     std::cout << clang_getFileName(file) << std::endl;
-    CXSourceLocation loc = clang_getLocation(unit, file, 3, 7);
+
+    unsigned line = std::stoi(argv[2]);
+    unsigned column = std::stoi(argv[3]);
+    CXSourceLocation loc = clang_getLocation(unit, file, line, column);
     CXCursor c = clang_getCursor(unit, loc);
-                std::cout << "Cursor kind: " << clang_getCursorDisplayName(c)
-                << "\t"<< clang_getCursorKindSpelling(clang_getCursorKind(c)) <<'\n';
+    std::cout << "Cursor kind: " << clang_getCursorDisplayName(c)
+    << "\t"<< clang_getCursorKindSpelling(clang_getCursorKind(c)) <<'\n';
+    //CXType type = clang_getCursorType(c);
+    //std::cout << "\n" << clang_getTypeSpelling(type) << std::endl;
+    std::cout << "\n" << type(c) << std::endl;
+    c = clang_getCursorSemanticParent(c);
+    std::cout << "Cursor kind: " << clang_getCursorDisplayName(c)
+    << "\t"<< clang_getCursorKindSpelling(clang_getCursorKind(c)) <<'\n';
 
 
 /*
