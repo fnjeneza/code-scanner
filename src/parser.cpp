d55733aefe13b82a6eed905357b9d677f1115038
Francois Njeneza <njenezamed@gmail.com>
2017-06-20 23:26:19 +0200

    add to_string. Convert CXString to std::string

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,5 +1,15 @@
 #include "parser.hpp"
 
+namespace
+{
+    std::string to_string(const CXString & cx_str)
+    {
+        std::string str     = clang_getCString(cx_str);
+        clang_disposeString(cx_str);
+        return str;
+    }
+} //anonymous namespace
+
 namespace code {
 namespace analyzer {
 Parser::Parser(const std::string &filename)
@@ -89,12 +99,8 @@ std::string Parser::type(const CXCursor &cursor)
     // cursor type
     CXType type = clang_getCursorType(cursor);
     // cursor type spelling
-    CXString spelling = clang_getTypeSpelling(type);
-    // cursor spelling as string
-    std::string spelling_str = clang_getCString(spelling);
-    // freed the CXString
-    clang_disposeString(spelling);
-    return spelling_str;
+    std::string spelling = to_string(clang_getTypeSpelling(type));
+    return spelling;
 }
 
 std::tuple<std::string, unsigned long, unsigned long>
@@ -108,20 +114,16 @@ Parser::location(const CXCursor &cursor)
     clang_getSpellingLocation(location, &file, &line, &column, nullptr);
 
     // filename
-    CXString    _filename = clang_getFileName(file);
-    std::string filename  = clang_getCString(_filename);
-    clang_disposeString(_filename);
+    std::string    _filename = to_string(clang_getFileName(file));
 
-    return std::make_tuple(filename, line, column);
+    return std::make_tuple(_filename, line, column);
 }
 
 std::string Parser::filename()
 {
     CXFile      file     = clang_getFile(m_unit, m_filename.c_str());
-    CXString    spelling = clang_getFileName(file);
-    std::string name     = clang_getCString(spelling);
-    clang_disposeString(spelling);
-    return name;
+    std::string    spelling = to_string(clang_getFileName(file));
+    return spelling;
 }
 
 } // namespace analyzer
