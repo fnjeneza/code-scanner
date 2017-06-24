a51c7aa896fabaedd1373d6b023c41897e457301
Francois Njeneza <njenezamed@gmail.com>
2017-06-24 09:56:31 +0200

    Use parser as fixture and  add locate implementation test

--- tests/test_parser.cpp
+++ tests/test_parser.cpp
@@ -3,19 +3,33 @@
 
 #include "parser.hpp"
 
-TEST_CASE("Locate cursor", "[location]")
+TEST_CASE("cpp parser", "[cpp_parser]")
 {
-    std::string filename("data/index.cpp");
+    std::string            filename("data/index.cpp");
     code::analyzer::Parser parser(filename);
-    unsigned long line =  11;
-    unsigned long column = 10;
-    CXCursor cursor  = parser.cursor(line, column);
 
-    auto loc = code::analyzer::location(cursor);
+    SECTION("retrieve cursor")
+    {
+    unsigned long          line   = 11;
+    unsigned long          column = 10;
+    CXCursor               cursor = parser.cursor(line, column);
+
+    auto loc          = code::analyzer::location(cursor);
     auto filename_ret = std::get<0>(loc);
-    auto line_ret = std::get<1>(loc);
-    auto column_ret = std::get<2>(loc);
+    auto line_ret     = std::get<1>(loc);
+    auto column_ret   = std::get<2>(loc);
     REQUIRE(filename_ret == filename);
     REQUIRE(line_ret == line);
     REQUIRE(column_ret == column);
+    }
+
+    SECTION("locate implementation")
+    {
+        auto cursor = parser.cursor(35, 10);
+        cursor = code::analyzer::reference(cursor);
+
+        auto cursor_expected = parser.cursor(17,13);
+        REQUIRE(clang_equalCursors(cursor, cursor_expected));
+    }
 }
+
