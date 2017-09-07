027a55aacb26e423940f9b70fb136506a11144ee
Francois Njeneza <njenezamed@gmail.com>
2017-09-07 16:47:52 +0200

    add InitializeParams

--- src/lib/include/code-scanner/code-scanner.hpp
+++ src/lib/include/code-scanner/code-scanner.hpp
@@ -45,6 +45,75 @@ struct ReferenceParams: public TextDocumentPositionParams {
   ReferenceContext context;
 };
 
+struct DynamicRegistration {
+  bool dynamicRegistration;
+};
+
+struct WorkspaceEdit {
+  bool documentChanges;
+};
+
+struct WorkspaceClientCapabilities{
+  bool applyEdit;
+  WorkspaceEdit workspaceEdit;
+  DynamicRegistration didChangeConfiguration;
+  DynamicRegistration didChangeWatchedFiles;
+  DynamicRegistration symbol;
+  DynamicRegistration executeCommand;
+};
+
+struct CompletionItem {
+  bool snippetSupport;
+};
+
+struct Completion : public DynamicRegistration{
+  CompletionItem completionItem;
+};
+
+struct Synchronization : public DynamicRegistration{
+  bool willSave;
+  bool WillSaveWaituntil;
+  bool didSave;
+};
+
+struct TextDocumentClientCapabilities {
+    Synchronization synchronization;
+    Completion completion;
+    DynamicRegistration hover;
+    DynamicRegistration signatureHelp;
+    DynamicRegistration references;
+    DynamicRegistration documentHighlight;
+    DynamicRegistration documentSymbol;
+    DynamicRegistration formatting;
+    DynamicRegistration rangeFormatting;
+    DynamicRegistration ontypeFormatting;
+    DynamicRegistration definition;
+    DynamicRegistration codeAction;
+    DynamicRegistration codeLens;
+    DynamicRegistration documentLink;
+    DynamicRegistration rename;
+};
+
+struct Capabilities {
+  WorkspaceClientCapabilities workspace;
+  TextDocumentClientCapabilities textDocument;
+  std::string experimental;
+};
+
+enum class Trace {
+  off,
+  messages,
+  verbose
+};
+
+struct InitializeParams {
+    std::size_t processId;
+    DocumentUri rootUri;
+    std::string initializationOptions;
+    Capabilities capabilities;
+    Trace trace;
+};
+
 struct Parser_Impl;
 
 class Parser{
