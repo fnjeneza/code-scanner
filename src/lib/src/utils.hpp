0a3db987fa3e0f3569d16cec1921e2e2d4b6910a
Francois Njeneza <njenezamed@gmail.com>
2017-09-11 17:45:43 +0200

    add missing utils.{cpp,hpp}

---
+++ src/lib/src/utils.hpp
@@ -0,0 +1,11 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include <string>
+#include <vector>
+
+namespace utils {
+std::vector<std::string> split(const std::string &argument);
+}
+
+#endif /* UTILS_HPP */
