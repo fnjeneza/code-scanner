0d5f6c2a299aa042aa84a8a4dccfbf643462be7f
Francois Njeneza <njenezamed@gmail.com>
2017-12-12 06:54:48 +0100

    add namespace

--- src/lib/include/code-scanner/ResponseError.hpp
+++ src/lib/include/code-scanner/ResponseError.hpp
@@ -2,8 +2,13 @@
 
 #include <string>
 
+namespace code {
+namespace analyzer {
+
 struct ResponseError
 {
     std::size_t code = 0;
     std::string message;
 };
+} // namespace analyzer
+} // namespace code
